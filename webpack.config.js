61e6a78a99c2406712d4d90a3ea872113b6d80ac
hir0naka <mizuguti06gmail.com>
2018-12-31 21:52:58 +0900

    make Library

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        open: true,
+        openPage: "index.html",
+        contentBase: path.join(__dirname, 'public'),
+        watchContentBase: true,
+        port: 8080,
+    },
+    entry: {app: './src/index.js'},
+    output: {
+        publicPath: "/js/",
+        filename: '[name].js',
+        library: ["com", "example"],
+        libraryTarget: 'umd'
+    }
+};
